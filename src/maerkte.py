cfec3c2c5b4e87a9c49ed64e31fd4b4bdcb12b24
fabi kohlrabi <fabiklein17@gmail.com>
2025-12-24 13:47:38 +0100

    ergänzung parametrisierung durch synth_maerkte_custom

--- src/maerkte.py
+++ src/maerkte.py
@@ -105,6 +105,71 @@ def synth_maerkte(df_maerkte: pd.DataFrame, maerkte_count: int) -> pd.DataFrame:
 
     return synthetic_maerkte
 
+def synth_maerkte_custom(df_maerkte: pd.DataFrame, maerkte_count: int, cluster_0_rel: float, cluster_1_rel: float, cluster_2_rel: float) -> pd.DataFrame:
+    '''Generierung von synthetischen Märkten auf Basis der geclusterten Märkte in kmeans_cluster_maerkte und eigenen Cluster Angaben.
+
+    Für jedes cluster wird auf Basis der cluster eingaben Märkten pro cluster zu generiert.
+    Es werden die Spalten 'diff_article', 'avg_kolli' & 'orders' als Grundlage für die Generierung mittels GaussianKDE verwendet.
+    Anschließend werden die neu erzeugten Werte gerundet, geclipped um Plausibilität der Werte zu gewährleisten
+    
+    Args:
+        df_maerkte (DataFrame): Cluster Marktdatensatz aus kmeans_cluster_maerkte
+        maerkte_count (Integer): Anzahl zu generierenden Märkten 
+        cluster_0_rel (Float): Relativen Anteil an der Gesamtheit an der zu generierenden Märktenvon cluster 0
+        cluster_1_rel (Float): Relativen Anteil an der Gesamtheit an der zu generierenden Märktenvon cluster 1
+        cluster_2_rel (Float): Relativen Anteil an der Gesamtheit an der zu generierenden Märktenvon cluster 2
+    
+    Returns:
+        synthetic_maerkte (DataFrame): Synthetischer Marktdatensatz'''
+
+    allowed_cluster = df_maerkte['cluster'].unique()
+
+    allowed_share = {
+        0: cluster_0_rel,
+        1: cluster_1_rel,
+        2: cluster_2_rel
+    }
+
+    synthetic_parts = []
+
+    for cluster_id in allowed_cluster:
+
+        df_maerkte_cluster = df_maerkte[df_maerkte['cluster'] == cluster_id]
+        
+        # Wie war das Verhältnis der cluster beim originalen Datensatz und wie wäre das bei den zu erzeugendn Märkten?
+        n_pro_cluster = int(round(maerkte_count * allowed_share[cluster_id]))
+
+        # Columns für Generierung
+        cols_for_model = df_maerkte_cluster[['diff_article', 'avg_kolli', 'orders']]
+
+        # Modell trainieren
+        model_maerkte = GaussianMultivariate(distribution=GaussianKDE)
+        model_maerkte.fit(cols_for_model)
+
+        # Maerkte generieren
+        synthetic_maerkte = model_maerkte.sample(n_pro_cluster)
+
+        # Plausibilität gewährleisten bei orders, avg_kolli & diff_article
+        synthetic_maerkte['orders'] = synthetic_maerkte['orders'].round().astype(int).clip(lower=1)
+        synthetic_maerkte['avg_kolli'] = synthetic_maerkte['avg_kolli'].round().astype(float).clip(lower=1)
+        synthetic_maerkte['diff_article'] = synthetic_maerkte['diff_article'].round().astype(int).clip(lower=1)
+
+        synthetic_maerkte['cluster'] = cluster_id
+
+        synthetic_parts.append(synthetic_maerkte)
+    
+    synthetic_maerkte = pd.concat(synthetic_parts)
+
+    synthetic_maerkte = synthetic_maerkte.reset_index(drop=True)
+
+    # Fake_Marktnummer generieren
+    synthetic_maerkte['Marktnummer'] = np.arange(1, len(synthetic_maerkte) + 1)
+
+    # Sortierung
+    synthetic_maerkte = synthetic_maerkte[['Marktnummer', 'diff_article', 'avg_kolli', 'orders', 'cluster']]
+
+    return synthetic_maerkte
+
 
 
 
