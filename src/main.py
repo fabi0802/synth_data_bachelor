f6784163644f855834bd7342c498ff0738c8dbf6
fabi kohlrabi <fabiklein17@gmail.com>
2026-04-07 02:18:07 +0200

    final änderungen

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from maerkte import kmeans_cluster_maerkte, synth_maerkte, synth_maerkte_custom
 from bestellungen import kmeans_cluster_bestellungen, synth_bestellungen, synth_bestellungen_custom
 from bestellpositionen import synth_bestellpositionen
-from analyse import  zusammenfassung_analysen_roh, avg_kolli_visual, orderlines_visual, diff_article_visual, tagesvolumen_visual, artikelvolumen_visual
+from analyse import  zusammenfassung_analysen_roh, avg_kolli_visual, orderlines_visual, diff_article_visual, tagesvolumen_visual, artikelvolumen_visual, korrelation_bestellungen, korrelation_maerkte
 from resampling import resampling_hochrechnung, resampling_maerkte, resampling_bestellungen
 
 jetzt = datetime.now()
@@ -24,24 +24,6 @@ df_neue_maerkte = synth_maerkte(
     )
 
 
-'''Alternative zur standard Markt Generierung (synth_maerkte):
-
-Aktiver Eingriff in die Markt Synthetisierung durch die Veränderung der relativen Markcluster Anteile.
-Relativer Anteil bestimmt, wie viele Märkte vom jeweiligen Cluster erstellt werden. 
-
-Kategorisierung für den Marktmix:
-cluster_0 = Drittkunden (geringe Artikelvielfalt, hohe Kolli Menge)
-cluster_1 = REWE Center (extrem hohe Artikelvielfalt, kleine Menge je Artikel)
-cluster_2 = Standard REWE (hohe Artikelvielfalt, viele Bestellungen, kleine Kolli Mengen)
-
-marktmix = {
-    0: 0.005,
-    1: 0.99,
-    2: 0.005
-}
-df_neue_maerkte = synth_maerkte_custom(df_maerkte, generiere_kunden, marktmix)'''
-
-
 # Bestehende Bestellungen zu Clustern zusammenfassen
 df_bestellungen = df.copy()
 df_bestellungen = kmeans_cluster_bestellungen(df_bestellungen)
@@ -52,23 +34,6 @@ df_neue_bestellungen = synth_bestellungen(
     df_bestellungen, 
     df_maerkte, 
     df_neue_maerkte)
-
-
-'''Alternative zur standard Bestellungen Generierung (synth_bestellungen):
-
-Aktiver Eingriff in die Bestell-Synthetisierung durch die Veränderung der relativen Bestellcluster Anteile.
-Relativer Anteil bestimmt, wie viele Bestellungen vom jeweiligen Cluster erstellt werden. 
-
-Kategorisierung für den Bestellmix:
-cluster_0 = Großbestellungen (weniger Bestellungen, breite Sortimente pro Bestellung)
-cluster_1 = Regelbestellungen (viele Bestellungen, geringe Menge pr Bestellung, breite Sortimente)
-
-bestellmix = {
-  0: {0: 0.1, 1: 0.9},
-  1: {0: 0.1, 1: 0.9},
-  2: {0: 0.1, 1: 0.9}
-}
-df_neue_bestellungen = synth_bestellungen_custom(df_bestellungen, df_maerkte, df_neue_maerkte, bestellmix)'''
 
 
 # Neue Bestellpositionen generieren & abspeichern
@@ -121,6 +86,23 @@ artikelvolumen_visual(
     df_resample
 )
 
+# Korrelation auf Marktebene vergleichen
+df_corr_maerkte = korrelation_maerkte(
+    df_maerkte,
+    df_neue_maerkte,
+    df_resample_maerkte
+)
+
+df_corr_maerkte.to_excel(f'reports/summary/corr_maerkte{jetzt:%Y_%m_%d_%H_%M}.xlsx')
+
+# Korrelation auf Bestellebene vergleichen
+df_corr_bestellungen = korrelation_bestellungen(
+    df_bestellungen,
+    df_neue_bestellungen,
+    df_resample_bestellungen
+    )
+
+df_corr_bestellungen.to_excel(f'reports/summary/corr_bestellungen{jetzt:%Y_%m_%d_%H_%M}.xlsx')
 
 # Kennzahlen zu Märkten, Bestellungen und Bestellpositionen
 roh_analyse = zusammenfassung_analysen_roh(
@@ -135,7 +117,5 @@ roh_analyse = zusammenfassung_analysen_roh(
 
 roh_analyse.to_excel(f'reports/summary/roh_analyse_{jetzt:%Y_%m_%d_%H_%M}.xlsx')
 
-# ABC-Analyse Vergleich zwischen real und real + synth orderlines & Analyse-Plot
-# Abgleich_abc_klasse = abc_analyse(df, df_neue_bestellpositionen)
 
 print('Durchlauf ist fertig')
